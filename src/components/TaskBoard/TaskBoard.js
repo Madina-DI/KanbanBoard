2c962c1ffd913c3713f7ab732507fdba11601dca
Madina_DI <12mileik@rambler.ru>
2024-12-16 23:12:48 +0300

    move task from backlog column to ready column

--- src/components/TaskBoard/TaskBoard.js
+++ src/components/TaskBoard/TaskBoard.js
@@ -3,16 +3,41 @@ import './TaskBoard.css';
 import TaskList from '../TaskList/TaskList';
 
 const TaskBoard = ({ data, setData }) => {
+
+    const moveTaskToReady = (taskId) => {
+        let taskToMove = null;
+        const updatedData = data.map((column) => {
+            if (column.title === 'backlog') {
+                taskToMove = column.issues.find((issue) => issue.id === taskId); //находим задачу которую нужно перенести из колонки backlog
+                return {
+                    ...column,
+                    issues: column.issues.filter((issue) => issue.id !== taskId), //исключаем ее из колонки backlog
+                };
+            } 
+
+            if (column.title === 'ready' && taskToMove) {
+
+                return {
+                ...column,
+                issues: [...column.issues, taskToMove], // Добавляем задачу в Ready
+                };
+            } 
+            return column; //остальные колонки остаются без изменений
+        }); 
+    setData(updatedData);// Обновляем глобальное состояние с помощью setData
+    }
  
 return (
     <div className='task-board'>
-        {data.map((column, index) => (
+        {data.map((column) => (
             <TaskList 
             key={column.title} 
             title={column.title} 
             tasks={column.issues} 
+            moveTaskToReady={moveTaskToReady}
             data={data}
             setData={setData}
+            backlogTasks={data.find((col) => col.title === 'backlog')?.issues || []}
             />
         ))}
     </div>
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import './TaskList.css';
 
-const TaskList = ({ title, tasks, data, setData }) => {
+const TaskList = ({ title, tasks, data, setData, moveTaskToReady, backlogTasks }) => {
     const [newTaskName, setNewTaskName] = useState('');
     const [isAddingTask, setIsAddingTask] = useState(false);
+    const [selectedTask, setSelectedTask] = useState('');
 
-    const handleAddTask = () => {
+    const handleMoveTask = () => {
+        if (selectedTask) {
+            moveTaskToReady(selectedTask); // Перемещаем задачу
+            setSelectedTask(''); // Сбрасываем выбранное значение
+        }
+
+        if (newTaskName.trim () === '') {
+            return;
+        }
         const newTask = {
             id: Date.now().toString(),
             name: newTaskName,
@@ -37,20 +46,39 @@ const TaskList = ({ title, tasks, data, setData }) => {
                     </div>
                 ))}
             </div>
+            {title === 'ready' && backlogTasks.length > 0 && (
+                <div className="task-list__dropdown">
+                    <select
+                        value={selectedTask}
+                        onChange={(e) => setSelectedTask(e.target.value)}
+                    >
+                        <option value="">Select a task</option>
+                        {backlogTasks.map((task) => (
+                            <option key={task.id} value={task.id}>
+                                {task.name}
+                            </option>
+                        ))}
+                    </select>
+                    <button onClick={handleMoveTask}>+Add card</button>
+                </div>
+            )}
+
             {title === 'backlog' && (
-                <div>
+                <div className='task-list__button'>
                     {isAddingTask ? (
-                        <div>
-                            <input
-                                type="text"
-                                placeholder="Введите название задачи"
-                                value={newTaskName}
-                                onChange={(e) => setNewTaskName(e.target.value)}
-                            />
-                            <button onClick={handleAddTask} className='task-list__addbutton-submit'>Submit</button>
+                        <div className='task-list__button-input'>
+                            <div>
+                                <input className='task-list__button-text' 
+                                    type="text"
+                                    placeholder="New task title..."
+                                    value={newTaskName}
+                                    onChange={(e) => setNewTaskName(e.target.value)}
+                                />
+                            </div>
+                            <button onClick={handleMoveTask} className='task-list__button-submit'>Submit</button>
                         </div>
                     ) : (
-                        <button onClick={() => setIsAddingTask(true)}>Add Card</button>
+                        <button onClick={() => setIsAddingTask(true)} className='task-list__button-add'>+Add Card</button>
                     )}
                 </div>
             )}
